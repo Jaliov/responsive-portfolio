efdf9f16c32cde4925ed2c3cdb0d6923b5d1445a
Joel Rudin <jarud55@gmail.com>
2026-04-18 16:55:13 -0400

    animation and image edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -114,7 +114,7 @@ const sr = ScrollReveal({
 })
 
 sr.reveal(`.home__data,  .home__social, contact__container, .footer__container`)
-sr.reveal(`.home__image`,  {origin: 'bottom'})
+sr.reveal(`.home__image, skills__group, .contact`, {origin: 'bottom'})
 sr.reveal(`.about__data, .skills__data`, {origin: 'left'})
-sr.reveal(`.about__image,  ,skills__content`, {origin: 'right'})
+sr.reveal(`.skills__content, .about__container`, {origin: 'right'})
 sr.reveal(`.services__card, .projects__card`, {interval: 100})
